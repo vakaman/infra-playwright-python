62702e64387b4f37c4c4c86e8795c57e94ccd673
Samuel Sublate <samuelsublate.dev@gmail.com>
2021-10-14 12:58:38 -0300

    subindo a infra responsavel por cuidar da execução do bot simples nacional

---
+++ src/init.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://google.com")
+    print(page.title())
+    browser.close()
